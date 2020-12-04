b8f17acab43463211969ff69f574a3dc9e326bcd
NataliiaMolch <nataliia.molchanova@epfl.ch>
2020-12-04 11:08:47 +0100

    Ignore .png

--- Project_2/GodunovFlux.m
+++ Project_2/GodunovFlux.m
@@ -1,9 +1,20 @@
-function Flux = GodunovFlux(A,absA,UL,UR)
+% function Flux = GodunovFlux(A,absA,UL,UR)
+% 
+% % N=size(UL, 2);
+% N = length(UL);
+% for i=1:N
+%     Flux(:,i) = 0.5*A(:,:,i)*(UL(:,i)+UR(:,i)) - 0.5*absA(:,:,i)*(UR(:,i)-UL(:,i));
+% end
+% 
+% end
 
-% N=size(UL, 2);
-N = length(UL);
+function Flux = GodunovFlux(absA,fL,fR,UL,UR)
+
+N=size(UL, 2);
+Flux = 0.5 * (fL + fR);
+
 for i=1:N
-    Flux(:,i) = 0.5*A(:,:,i)*(UL(:,i)+UR(:,i)) - 0.5*absA(:,:,i)*(UR(:,i)-UL(:,i));
+    Flux(:, i) = Flux(:, i) - 0.5*absA(:, :, i+1)*(UR(:, i)-UL(:, i));
 end
 
 end
--- Project_2/Lax_Friedrich_2D.m
+++ Project_2/Lax_Friedrich_2D.m
@@ -1,17 +1,17 @@
-function [F,alpha_LF] = Lax_Friedrich_2D(q,g)
+function [F,alpha_LF] = Lax_Friedrich_2D(q,fL,fR,UL,UR,g)
 %LAX_FRIEDRICH_2D Local lax friederich flux
 
-f = flux_func(q, g);
-u = q(2,:)/q(1,:);
-c = sqrt(abs(g*q(1,:)));
+q = q(:,2:end-1);
+% u = q(2,:)/q(1,:);
+% c = sqrt(abs(g*q(1,:)));
 % e_val1 = abs(u + c); e_val2 = abs(u - c);
 % 
 % alpha = max(max(e_val1(1:end-1), max(e_val1(2:end))), max(e_val2(1:end-1), max(e_val2(2:end))));
-max_eig_val = abs(abs(u)+c);
+max_eig_val = abs(abs(q(2,:)/q(1,:))+sqrt(abs(g*q(1,:))));
 alpha = max(max_eig_val(1:end-1), max_eig_val(2:end));
 alpha_LF = [alpha;alpha];
 
-F = (f(:,1:end-1) + f(:,2:end))/2 - alpha_LF/2.*(q(:,2:end) - q(:,1:end-1));
+F = (fL + fR)/2 - alpha_LF/2.*(UR - UL);
 
 end
 
